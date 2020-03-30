60392d7725c36277ebd04b63c23b33afe5e2f5a4
Dan McKinney <3dhdaniel@gmail.com>
2020-03-30 14:19:03 -0400

    mor

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,12 +108,26 @@ router.get('/', async (req, res) => {
 router.get('/user/:user_id', async (req, res) => {
     try {
         const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
-        if(!profile) return res.status(400).json({msg: 'There is no profile for this user'})
+        if(!profile) 
+            return res.status(400).json({msg: 'There is no profile for this user'})
         res.json(profile)
     } catch (err) {
         console.error(err.message);
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({msg: 'There is no profile for this user'})
+        }
         res.status(500).send('Server messd up')
     }
 })
 
+router.delete('/', auth, async (req, res) => {
+    try {
+        await Profile.findByIdAndRemove({ user: req.user.id});
+        await User.findByIdAndRemove({ _id: req.user.id})
+        res.json({ msg: 'user removed'})
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('server error')
+    }
+})
 module.exports = router;
